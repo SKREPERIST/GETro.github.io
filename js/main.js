160accf06ebc05e9b3059f7d570848f434bedd3b
SKREPERIST <skreperist666@gmail.com>
2019-09-24 18:28:22 +0300

    starting

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+
+  $('.slider__big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.slider__small'
+  });
+  $('.slider__small').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.slider__big',
+    variableWidth: true,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+  })
+
+
+  $('.header__menu-btn').on('click', function () {
+    $('.header__menu > a').slideToggle();
+  });
+
+
+
+
+
+});
