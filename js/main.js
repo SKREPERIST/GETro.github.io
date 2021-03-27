16c32d3623aff5c8ea786d75d2c3cd189ed5b558
SKREPERIST <skreperist666@gmail.com>
2021-03-27 13:28:15 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 $(function () {
-
-  $('.slider__big').slick({
+  $(".slider__big").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    asNavFor: '.slider__small'
+    asNavFor: ".slider__small",
   });
-  $('.slider__small').slick({
+  $(".slider__small").slick({
     slidesToShow: 6,
     slidesToScroll: 1,
-    asNavFor: '.slider__big',
+    asNavFor: ".slider__big",
     variableWidth: true,
     dots: false,
     centerMode: true,
     focusOnSelect: true,
-  })
-
-
-  $('.header__menu-btn').on('click', function () {
-    $('.header__menu > a').slideToggle();
   });
 
+  $(".header__menu-btn").on("click", function () {
+    $(".header__menu > a").slideToggle();
+  });
 
+  let btn = document.querySelector(".header__menu-btn");
 
-
-
+  btn.onclick = () => {
+    btn.classList.toggle("active");
+  };
 });
